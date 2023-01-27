8ddcb337cc0c92e1dad8fb5c46417aac9a2edcb2
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-26 21:46:04 -0700

    Primer arreglo foreach en java
    Signed-off-by: Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>

---
+++ EstructuraforEach/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //Ejercicio para utilizar el foreach, es un ciclo de recorrimiento
+        //Se usa principalmente para recorrer arreglos o listas
+        //De 
+        String[] nombres = {"Angel","Ruben","Maria", "Jessica","Osvaldo"};
+        for (String elementos : nombres) {
+            System.out.println("Nombre" + elementos);
+            
+        }
+
+
+    }
+}
